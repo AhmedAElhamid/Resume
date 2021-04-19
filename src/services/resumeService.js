ebe2f77b38315079ddc2eec231ce996ee4b36a11
Ahmed Abdelhamid <42896530+AhmedAElhamid@users.noreply.github.com>
2021-04-19 20:30:48 +0200

    Initial commit

--- src/services/resumeService.js
+++ src/services/resumeService.js
@@ -1,8 +1,6 @@
 import http from './httpService'
-import config from '../config/config.json'
 
-const moviesEndPoint = config.apiEndPoint
 export function getResume(){
-    return http.get(moviesEndPoint)
+    return http.get("/resume")
 }
 
